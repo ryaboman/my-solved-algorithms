288a103f374342ba4c19562b693933be024b9e26
Aleksey <mr.ryabov.aa@yandex.ru>
2025-01-25 11:33:25 +0300

    Добаляет задачи A, B с Яндекс.Контекст

---
+++ algorithms/src/main/java/test_yandex/test_5.java
@@ -0,0 +1,61 @@
+package test_yandex;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class test_5 {
+    public static void main(String[] args) {
+        char[][] matrix;
+        List<String> words = new ArrayList<String>();
+        try {
+            Scanner in = new Scanner(new File("input.txt"));
+            int r = in.nextInt();
+            int c = in.nextInt();
+
+            matrix = new char[r][c];
+            int i = 0;
+            while (in.hasNext()) {
+                String row = in.nextLine();
+                if (row.length() == 0) {
+                    continue;
+                }
+                words.addAll( Arrays.asList(row.split("#")) );
+                matrix[i] = row.toCharArray();
+                i++;
+            }
+
+            for(int j=0; j < c; j++) {
+                char[] col = new char[r];
+                for (int k = 0; k < col.length; k++) {
+                    col[k] = matrix[k][j];
+                }
+                String column = new String(col);
+                words.addAll(Arrays.asList(column.split("#")));
+            }
+
+            Collections.sort(words);
+
+            String search_word = "";
+            for(var word : words) {
+                if(word.length() >= 2){
+                    search_word = word;
+                    break;
+                }
+
+            }
+
+            File output = new File("output.txt");
+            PrintWriter pw = new PrintWriter(output);
+            pw.print(search_word);
+            pw.close();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
---
+++ algorithms/src/main/java/test_yandex/test_6.java
@@ -0,0 +1,59 @@
+package test_yandex;
+
+import java.util.*;
+
+public class test_6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int b = in.nextInt();
+        int[] arr = new int[n];
+        int index_b = -1;
+        for (int arr_i = 0; arr_i < n; arr_i++) {
+            int x = in.nextInt();
+            if (x > b){
+                arr[arr_i] = 1;
+            }
+            else if (x < b){
+                arr[arr_i] = -1;
+            }
+            else {
+                index_b = arr_i;
+                arr[arr_i] = 0;
+            }
+
+        }
+
+        int[] P = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            P[i+1] = P[i] + arr[i];
+        }
+
+        Map<Integer, Integer[]> map = new HashMap<>();
+        for (int i = 0; i <= index_b; i++) {
+            int parity = i % 2;
+            if (map.containsKey(P[i])) {
+                map.get(P[i])[parity]++;
+            }
+            else {
+                Integer[] arrParity = new Integer[2];
+                arrParity[0] = 0;
+                arrParity[1] = 0;
+                arrParity[parity]++;
+                map.put(P[i], arrParity);
+            }
+        }
+
+        int ans = 0;
+        for(int i = index_b; i < n; i++){
+            int sum = P[i+1];
+            int parity = i % 2;
+            if (map.containsKey(sum)) {
+                ans += map.get(sum)[parity];
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
