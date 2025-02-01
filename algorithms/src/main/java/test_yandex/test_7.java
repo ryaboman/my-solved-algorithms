640db37b259d31d0abf5a806905294a6528403fe
Aleksey <mr.ryabov.aa@yandex.ru>
2025-02-01 23:20:47 +0300

    Добавляет решение задачи C test_7 с Яндекс.Контекст

---
+++ algorithms/src/main/java/test_yandex/test_7.java
@@ -0,0 +1,64 @@
+package test_yandex;
+
+import java.util.*;
+
+public class test_7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr = new int[3][n];
+
+        for (int j = 0; j < 3; j++){
+            for (int i = 0; i < n; i++) {
+                arr[j][i] = in.nextInt();
+            }
+        }
+
+        Map<Integer, List<Integer>[]> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(arr[0][i], new List[]{new ArrayList<>(), new ArrayList<>()});
+        }
+
+        for (int i = 0; i < n; i++) {
+            int x1 = arr[1][i];
+            if (map.containsKey(x1)) {
+                map.get(x1)[0].add(i);
+            }
+            int x2 = arr[2][i];
+            if (map.containsKey(x2)) {
+                map.get(x2)[1].add(i);
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if(ans.contains(i)){
+                continue;
+            }
+            int key = arr[0][i];
+            if( map.get(key)[0].size() == 0 || map.get(key)[1].size() == 0 ){
+                //System.out.println("null in row i=" + i);
+                ans.add(i);
+
+                int valueKey1 = arr[1][i];
+                int valueKey2 = arr[2][i];
+
+                if(valueKey1 != key){
+                    int indexToRemove = map.get(valueKey1)[0].indexOf(i);
+                    map.get(valueKey1)[0].remove(indexToRemove);
+                }
+
+                if(valueKey2 != key){
+                    int indexToRemove = map.get(valueKey2)[1].indexOf(i);
+                    map.get(valueKey2)[1].remove(indexToRemove);;
+                }
+
+                if(valueKey1 != key || valueKey2 != key){
+                    i = -1;
+                }
+            }
+        }
+        System.out.println(ans.size());
+    }
+}
