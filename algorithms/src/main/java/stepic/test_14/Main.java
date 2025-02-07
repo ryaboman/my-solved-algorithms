e4b8471e97d85133da95717a9f9a0224bdf56c34
Aleksey <mr.ryabov.aa@yandex.ru>
2025-02-07 18:17:09 +0300

    Добавляет задачу реализации функции работающей с лямбда-выражением

---
+++ algorithms/src/main/java/stepic/test_14/Main.java
@@ -0,0 +1,21 @@
+package stepic.test_14;
+
+import java.util.Objects;
+import java.util.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Predicate<Object> condition = Objects::isNull;
+        Function<Object, Integer> ifTrue = obj -> 0;
+        Function<CharSequence, Integer> ifFalse = CharSequence::length;
+        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+        System.out.println(safeStringLength.apply("hello"));
+    }
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        return t -> condition.test(t) ? ifTrue.apply(t) : ifFalse.apply(t);
+    }
+}
